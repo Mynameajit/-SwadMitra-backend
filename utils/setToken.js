f5b16a7a428cefe1618d56c406ee15b5711edfe6
project-2003 <project154321@gmail.com>
2025-12-28 17:17:27 +0530

    Fix token set bugs and improve order flow

--- utils/setToken.js
+++ utils/setToken.js
@@ -1,20 +1,19 @@
 import jwt from 'jsonwebtoken';
 
-export const setToken = (user, res, statusCode, message) => {
+export const setToken = async (user, res, statusCode, message) => {
 
     try {
 
-        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+        const token = await jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
             expiresIn: process.env.JWT_EXPIRE
         })
 
-        // localhost पर काम करने वाला config
         res.cookie("token", token, {
             httpOnly: true,
-            secure: false,
-            sameSite: "Lax",
+            secure: true,       
+            sameSite: "None",  
             path: "/",
-            expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000), // 7 days
+            maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
         });
 
         res.status(statusCode).json({
